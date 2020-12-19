8c9b99d815930a84049f592c1d1ba3d0fcb95cbb
Ramendra Namdev <30554691+ramendranamdev@users.noreply.github.com>
2020-12-19 18:25:10 +0530

    Covid Data Added covid data api

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ export default function App() {
   return (
     <div className="App">
       <State/>
-      <h2>Hello CodeSandbox</h2>
-      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
--- src/State.js
+++ src/State.js
@@ -2,38 +2,47 @@ import Axios from "axios";
 import React, { Component } from "react";
 
 export class State extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lists: []
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			lists: [],
+			baseUrl:
+				"https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true"
+		};
+	}
 
-  componentDidMount() {
-    Axios.get("https://reqres.in/api/users?page=1")
-      .then((response) => response.data)
-      .then((res) => {
-        let { data } = res;
-        let local = [];
-        
-        data.forEach(x=>{
-          local.push(x['email']);
-        })
+	componentDidMount() {
+		Axios.get(this.state.baseUrl)
+			.then((response) => response.data)
+			.then((res) => {
+				var { regionData } = res;
+				this.setState({
+					lists: regionData
+				});
+			});
+	}
 
-        this.setState({
-            lists: local
-          });
-      });
-  }
-
-  render() {
-    return (
-        <h1>
-          {this.state.lists[1]}
-          hI
-        </h1>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<hr />
+				{this.state.lists.map((x, index) => {
+					return (
+						<div key={index}>
+							<h1>{x.region}</h1>
+							<h4>Total Infected: {x.totalInfected}</h4>
+							<h4>New Infected: {}</h4>
+							<h4>Recovered: {x.recovered}</h4>
+							<h4>New Recovered: {x.newRecovered}</h4>
+							<h4>Deceased: {x.deceased}</h4>
+							<h4>New Deceased: {x.newDeceased}</h4>
+							<hr />
+						</div>
+					);
+				})}
+			</div>
+		);
+	}
 }
 
 export default State;
